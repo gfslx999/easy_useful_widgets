45a0a9f3ff25263378884cb2526880296563fa11
fengseng.gao <https://gitlab.hexcloud.cn/dashboard/projects>
2022-08-12 17:13:30 +0800

    增加-example示例

--- lib/src/main/overstory.dart
+++ lib/src/main/overstory.dart
@@ -1,8 +1,16 @@
 /// 防重复点击/防抖/节流
 ///
 /// [action] 要执行的方法
-/// [intervalDuration] 间隔多长时间执行
-dynamic debounce(Function? action, {
+/// [intervalDuration] 间隔多长时间执行，默认1秒
+///
+/// 'example':
+///     InkWell(
+///       onTap: debounce(() {
+///         // 这里最快的回调速度为 1秒/次
+///       }, intervalDuration: const Duration(seconds: 1)),
+///       child: const Text('Test Click'),
+///     );
+dynamic debounce(Function action, {
   Duration intervalDuration = const Duration(seconds: 1),
 }) {
   // 上次点击生效的时间, 单位：毫秒
@@ -14,9 +22,7 @@ dynamic debounce(Function? action, {
     final currentTime = DateTime.now().millisecondsSinceEpoch;
     // 判断当前时间与上次点击时间是否已经大于了指定间隔
     if (currentTime - lastTakeEffectClickTime >= intervalDuration.inMilliseconds) {
-      if (action != null) {
-        action();
-      }
+      action();
       // 更新点击生效时间时间
       lastTakeEffectClickTime = currentTime;
     }
